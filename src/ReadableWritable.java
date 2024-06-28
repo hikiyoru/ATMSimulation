530c49394b6cc099d8501a2eac305cbd4680e7f7
Dmitriy Glotov <uglotov18@gmail.com>
2024-06-28 20:57:44 +0300

    ReadableWritable interface (Card -> Map<String, Card>)

--- src/ReadableWritable.java
+++ src/ReadableWritable.java
@@ -1,4 +1,6 @@
-public interface ReadableWritable<T extends ATM, J extends Card> {
+import java.util.Map;
+
+public interface ReadableWritable<T extends ATM, J extends Map<String, Card>> {
     void read(T t);
     void read(J j);
     void write(T t);
